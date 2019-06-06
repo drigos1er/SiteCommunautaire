6cc7f339b3427ed0d134d7adcde49142a88ee085
drigos1er <drigos1er@yahoo.fr>
2019-06-06 22:20:43 +0200

    HOME PAGE#1

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class HomeController extends AbstractController
+{
+
+
+
+    public function index()
+    {
+        return  $this->render('view/home.html.twig');
+    }
+
+
+
+}
