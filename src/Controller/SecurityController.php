6a370523ce2c372ab9075a8181bec9e4ca12aef9
drigos1er <drigos1er@yahoo.fr>
2019-06-13 20:40:19 +0200

    REGISTER PAGE #1

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\AuthenticatedUser;
+use App\Form\RegistrationType;
+use App\Repository\FiguresRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class SecurityController extends AbstractController
+{
+
+
+    public function registration(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+    {
+       $useraut= new AuthenticatedUser();
+       $form= $this->createForm(RegistrationType::class, $useraut);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hashpwd=$encoder->encodePassword($useraut, $useraut->getPassword());
+            $datecreate = new \Datetime();
+            $useraut->setPassword($hashpwd);
+            $useraut->setCreatedate($datecreate);
+            $useraut->setUpdatedate($datecreate);
+
+            $manager->persist($useraut);
+            $manager->flush();
+
+
+        }
+       return $this->render('security/registration.html.twig', array('form'=>$form->createView()));
+    }
+
+}
